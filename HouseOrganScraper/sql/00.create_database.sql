7b163d29a2d880702a90808e64efff549a4a92f8
pluselc <ij4nu8d4fw@gmail.com>
2015-10-25 00:20:42 +0900

    program development environment

---
+++ HouseOrganScraper/sql/00.create_database.sql
@@ -0,0 +1,12 @@
+-- Database: employee_tools
+
+-- DROP DATABASE employee_tools;
+
+CREATE DATABASE employee_tools
+  WITH OWNER = postgres
+       ENCODING = 'UTF8'
+       TABLESPACE = pg_default
+       LC_COLLATE = 'Japanese_Japan.932'
+       LC_CTYPE = 'Japanese_Japan.932'
+       CONNECTION LIMIT = -1;
+
