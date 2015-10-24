780d4eea7b3e2c8c4fb9a4f5b54ecc2e94a587c7
pluselc <ij4nu8d4fw@gmail.com>
2015-10-25 01:32:05 +0900

    setting database

---
+++ HouseOrganScraper/database/00.create_database.sql
@@ -0,0 +1,12 @@
+-- Database: employee_tool
+
+-- DROP DATABASE employee_tool;
+
+CREATE DATABASE employee_tool
+  WITH OWNER = postgres
+       ENCODING = 'UTF8'
+       TABLESPACE = pg_default
+       LC_COLLATE = 'Japanese_Japan.932'
+       LC_CTYPE = 'Japanese_Japan.932'
+       CONNECTION LIMIT = -1;
+
